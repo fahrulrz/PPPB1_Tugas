4df9ed7701b5f8221f1af5d9d4aa233f838f6eb6
fahrulrz <mfahrulrazi695@gmail.com>
2024-10-28 01:03:07 +0700

    BNI Mobile

--- BNIMobile/app/src/main/java/com/example/bnimobile/MainActivity.kt
+++ BNIMobile/app/src/main/java/com/example/bnimobile/MainActivity.kt
@@ -5,8 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.bnimobile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
