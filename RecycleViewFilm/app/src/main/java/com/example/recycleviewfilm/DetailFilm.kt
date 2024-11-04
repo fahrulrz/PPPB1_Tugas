7a16c07a179b398643d9cc16f80d057848cad3d4
fahrulrz <mfahrulrazi695@gmail.com>
2024-11-04 13:23:31 +0700

    add btn function

--- RecycleViewFilm/app/src/main/java/com/example/recycleviewfilm/DetailFilm.kt
+++ RecycleViewFilm/app/src/main/java/com/example/recycleviewfilm/DetailFilm.kt
@@ -1,41 +1,58 @@
 package com.example.recycleviewfilm
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
-//import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.recycleviewfilm.databinding.FilmCardBinding
+import com.example.recycleviewfilm.databinding.ActivityDetailFilmBinding
 
 class DetailFilm : AppCompatActivity() {
-    private lateinit var binding: DetailFilm
+    private lateinit var binding: ActivityDetailFilmBinding
     @SuppressLint("SuspiciousIndentation")
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_film)
+        binding = ActivityDetailFilmBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val movie = intent.getParcelableExtra("movie", Movie::class.java)
-
             if (movie != null) {
-            val filmTitle = findViewById<TextView>(R.id.film_title)
-            val duration = findViewById<TextView>(R.id.duration)
-            val ageRating = findViewById<TextView>(R.id.age_rating)
-            val filmCover = findViewById<ImageView>(R.id.film_cover)
-            val synopsis = findViewById<TextView>(R.id.synopsis)
+                with(binding){
+                    val filmTitle = findViewById<TextView>(R.id.film_title)
+                    val duration = findViewById<TextView>(R.id.duration)
+                    val ageRating = findViewById<TextView>(R.id.age_rating)
+                    val filmCover = findViewById<ImageView>(R.id.film_cover)
+                    val synopsis = findViewById<TextView>(R.id.synopsis)
 
-            filmTitle.text = movie.filmTitle
-            duration.text = movie.duration
-            ageRating.text = movie.ageRating
-            filmCover.setImageResource(movie.cover)
-            filmTitle.text = movie.filmTitle
-            synopsis.text = movie.synopsis
+                    filmTitle.text = movie.filmTitle
+                    duration.text = movie.duration
+                    ageRating.text = movie.ageRating
+                    filmCover.setImageResource(movie.cover)
+                    filmTitle.text = movie.filmTitle
+                    synopsis.text = movie.synopsis
+
+                    val includedLayout = findViewById<View>(R.id.nav_detail)
+                    val btnBack = includedLayout.findViewById<ImageView>(R.id.btn_back)
+                    btnBack.setOnClickListener {
+                        val intentToMainActivity = Intent(this@DetailFilm, MainActivity::class.java)
+                        startActivity(intentToMainActivity)
+                        finish()
+                    }
+
+                    btnBook.setOnClickListener {
+                        val intentToMainActivity = Intent(this@DetailFilm, MainActivity::class.java)
+                        startActivity(intentToMainActivity)
+                        finish()
+                    }
+                }
+
         }
+
+
     }
 }
